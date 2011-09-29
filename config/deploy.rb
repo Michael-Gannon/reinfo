eba96c8fc32382bad1bc0d8a196124eb11df0d6f
Michael Gannon <mikgan@Michael-Gannons-MacBook-Pro.local>
2011-09-29 10:36:50 +1000

    fiddled with vlad trying to deploy for the first time

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,13 @@
+require 'bundler/vlad'
+
 set :application, "reinfo"
 set :repository, "git@github.com:Michael-Gannon/reinfo.git"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-set :deploy_to, "/tmp/#{application}"
-
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-set :scm, 'git'
-
 set :user, 'mikgan'
+set :deploy_to, "/tmp/#{application}"
+set :scm, 'git'
+set :domain, 'mikgan.com'
+set :skip_scm, false
 
-#role :app, "your app-server here"
-#role :web, "your web-server here"
-#role :db,  "your db-server here", :primary => true
-
-set :host, 'mikgan'
+namespace :vlad do
+  task :deploy => [:update]
+end
