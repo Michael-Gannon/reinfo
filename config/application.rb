19ca2a370844b13fc295bf9b7a9e268ad85b8ae8
Michael Gannon <mikgan@Michael-Gannons-MacBook-Pro.local>
2011-09-29 10:36:24 +1000

    moved path for searchable plugin

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'lib/searchable'
+require File.join(__FILE__, '../../lib/searchable')
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
